f509db81115162f28b2f99635b7ea08d08e6db58
yahav <yahavxx@gmail.com>
2020-04-28 15:48:07 +0300

    Cmd arguments parse

--- main.cpp
+++ main.cpp
@@ -4,17 +4,20 @@
 #include "src/utils/UtilFunctions.h"
 #include "src/simulator/Simulator.h"
 
+void parseCmdArguments(int argc, char **argv, std::string &travelPath, std::string &algorithmPath, std::string &outputPath);
 
 int main(int argc, char **argv) {
-    if (argc < 3) {
+    if (argc != 3 && argc != 5 && argc != 7) {
         std::cerr << "Insufficient arguments supplied. Please read the README for usage instructions. Program is terminated." << std::endl;
         return 1;
     }
 
-    std::string outputDir = argv[1];
+    std::string travelPath = "", algorithmPath = "", outputPath = "";
 
-    if (!isDirectoryExists(outputDir)) {  // the output directory doesn't exist
-        bool created = createFolder(outputDir);  // try to create it
+    parseCmdArguments(argc, argv, travelPath, algorithmPath, outputPath);
+
+    if (!isDirectoryExists(outputPath)) {  // the output directory doesn't exist
+        bool created = createFolder(outputPath);  // try to create it
         if (!created) {
             std::cerr << "Couldn't initialize output directory. Please read the README for usage instructions. Program is terminated." << std::endl;
             return 1;
@@ -22,8 +25,8 @@ int main(int argc, char **argv) {
     }
 
     // Direct log prints to log files
-    std::string outputLogFile = outputDir + "/output.txt";
-    std::string errorLogFile =  outputDir + "/error.txt";
+    std::string outputLogFile = outputPath + "/output.txt";
+    std::string errorLogFile =  outputPath + "/error.txt";
     freopen(outputLogFile.c_str(), "w", stdout);  // TODO
     freopen(errorLogFile.c_str(), "w", stderr);
 
@@ -33,9 +36,28 @@ int main(int argc, char **argv) {
         travels.push_back(argv[i]);
     }
 
-    Simulator simulator(outputDir);
+    Simulator simulator(outputPath);
 
     simulator.runSimulations(travels);
 
     return 0;
+}
+
+void parseCmdArguments(int argc, char **argv, std::string &travelPath, std::string &algorithmPath, std::string &outputPath) {
+    for (int i = 1; i < argc; i += 2) {
+        std::string flag = argv[i];
+
+        if (flag == "-travel_path") {
+            travelPath = argv[i+1];
+        }
+
+        if (flag == "-algorithm_path") {
+            algorithmPath = argv[i+1];
+        }
+
+        if (flag == "-output") {
+            outputPath = argv[i+1];
+        }
+
+    }
 }
